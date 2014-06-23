f943c95eb791f4a17ac32fd139e1dfbdc4ab5e9a
Niccolo <nico@niccolosanarico.com>
2014-06-23 23:29:31 +0200

    fixing bootstrap missing fonts

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Coalcashop
     end
 
     # trying to fix bootstrap fonts
-    config.assets.paths << "#{Rails}/vendor/assets/fonts"
+    # config.assets.paths << "#{Rails}/vendor/assets/fonts"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
