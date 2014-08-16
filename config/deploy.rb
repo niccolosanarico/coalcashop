16b89655c4ea7f6a1b626396f959a51ac6c07f67
Niccolo <nico@niccolosanarico.com>
2014-08-16 17:00:12 +0200

    Adding puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,3 +8,6 @@ set :keep_releases, 5
 
 set :puma_init_active_record, true
 set :puma_bind, "unix:///tmp/coalcashop.socket"
+set :puma_workers, 2
+set :puma_access_log, "#{current_path}/log/puma_error.log"
+set :puma_error_log, "#{current_path}/log/puma_access.log"
