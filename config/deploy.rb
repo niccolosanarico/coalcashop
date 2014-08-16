d39f1b6748285265925d6de59db5b619a6df2dda
Niccolo <nico@niccolosanarico.com>
2014-08-16 18:23:59 +0200

    substituted puma for unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,10 +14,10 @@ set :puma_bind, "unix:///tmp/coalcashop.socket"
 set :puma_threads, [0, 16]
 set :puma_workers, 2
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "/tmp/puma.pid"
-set :puma_access_log, "#{shared_path}/log/puma_error.log"
-set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_state, -> { "#{shared_path}/tmp/pids/puma.state" }
+set :puma_pid, -> { "#{shared_path}/tmp/pids/puma.pid"}
+set :puma_access_log, -> { "#{shared_path}/log/puma_error.log" }
+set :puma_error_log, -> { "#{shared_path}/log/puma_access.log" }
 set :puma_role, :app
 set :puma_env, fetch(:rack_env, fetch(:rails_env, 'staging'))
 set :puma_init_active_record, true
