c5057500a2b87e94ce59b676885116b4967178ce
Niccolo <nico@niccolosanarico.com>
2014-08-16 17:56:44 +0200

    substituted puma for unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,8 +15,7 @@ set :puma_threads, [0, 16]
 set :puma_workers, 2
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_pid, "/tmp/puma.pid"
 set :puma_access_log, "#{shared_path}/log/puma_error.log"
 set :puma_error_log, "#{shared_path}/log/puma_access.log"
 set :puma_role, :app
