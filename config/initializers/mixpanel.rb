5da163b9917b3504c1a7336fbe48af21d63391f0
Niccolo <nico@niccolosanarico.com>
2014-09-08 22:13:10 +0200

    configuring mixpanel

--- config/initializers/mixpanel.rb
+++ config/initializers/mixpanel.rb
@@ -1,4 +1,4 @@
 Spree::Mixpanel::Config.configure do |config|
- config.connection_token = 'YOUR TOKEN'
+ config.connection_token = '38aefb55a006c8469fb9cb775b2f6da5'
  config.push_order_charges = true
 end
