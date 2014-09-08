911acb0e313175e029aecbf1b2543d86ff4416de
Niccolo <nico@niccolosanarico.com>
2014-09-08 23:48:23 +0200

    Fixing Mixpanel

--- config/initializers/mixpanel.rb
+++ config/initializers/mixpanel.rb
@@ -1,5 +1,5 @@
-require 'spree_mixpanel'
-require 'spree_mixpanel/engine'
+#require 'spree_mixpanel'
+#require 'spree_mixpanel/engine'
 
 Spree::Mixpanel::Config.connection_token = '38aefb55a006c8469fb9cb775b2f6da5'
 Spree::Mixpanel::Config.push_order_charges = true
