242bcf27b7d9020c18100a7cdc1be5a4f7a61a3d
ilnico <nico@niccolosanarico.com>
2015-01-13 11:51:47 +0100

    I18N fixes

--- app/overrides/add_IVA_to_product_details.rb
+++ app/overrides/add_IVA_to_product_details.rb
@@ -1,8 +1,14 @@
 Deface::Override.new(:virtual_path => 'spree/products/_cart_form',
                      :name => 'add_IVA_to_product_details',
-                     :replace => 'span.price',
+                     :replace => 'div#product-price span.price',
                      :text => '
                         <span class="lead price selling" itemprop="price">
                           <%= display_price(@product) %> <span class="iva-overview">+ IVA 22%</span>
                         </span>
                     ')
+
+Deface::Override.new(:virtual_path => 'spree/products/_cart_form',
+                    :name => 'fix_variants_list',
+                    :add_to_attributes => 'ul.list-group',
+                    :attributes => { :style => "list-style-type: none;"}
+                    )
--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,8 @@ module Coalcashop
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    I18n.config.enforce_available_locales = false
+    #I18n.config.enforce_available_locales = false
+    config.i18n.enforce_available_locales = false
     config.i18n.available_locales = [:en, :it]
     config.i18n.default_locale = :it
   end
