ed01677100c6f4811e710adc09f90c5658f3db2c
ilnico <nico@niccolosanarico.com>
2015-01-13 13:56:27 +0100

    working spree 2.3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery-ui
 //= require jquery_ujs
+//= require jquery-ui
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -2,17 +2,18 @@ Spree::CheckoutController.class_eval do
   # Updates the order and advances to the next state (when possible.)
 
     def update
-      if @order.update_from_params(params, permitted_checkout_attributes)
-        persist_user_address
+      if @order.update_from_params(params, permitted_checkout_attributes, request.headers.env)
+        @order.temporary_address = !params[:save_user_address]
         unless @order.next
           flash[:error] = @order.errors.full_messages.join("\n")
           redirect_to checkout_state_path(@order.state) and return
         end
 
         if @order.completed?
-          session[:order_id] = nil
+
+          @current_order = nil
           flash.notice = "Il tuo ordine e' stato completato con successo, tra poco riceverai una email con tutti i dettagli. Grazie per l'acquisto!"
-          flash[:commerce_tracking] = "nothing special"
+          flash['order_completed'] = true
           redirect_to completion_route
         else
           redirect_to checkout_state_path(@order.state)
