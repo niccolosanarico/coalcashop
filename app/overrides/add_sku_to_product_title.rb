5cbee8af855369e7db3af0e21bb265c6e2a401af
ilnico <nico@niccolosanarico.com>
2015-02-01 22:39:00 +0000

    Removed SKU from product title, it does not work

--- app/overrides/add_sku_to_product_title.rb
+++
@@ -1,7 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/products/show',
-                     :name => 'add_sku_to_product_title',
-                     :insert_after => 'h1.product-title',
-                     :text => %q(
-                      <h4 class="sku-subtitle">SKU: <%= @product.sku %></h4>
-                     )
-                    )
