f03683e2987e4c7ab0471b7a0159fae78a3b7a01
ilnico <nico@niccolosanarico.com>
2015-02-04 21:59:03 +0000

    Added subscription / unsubscription to newsletter for existing user

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,19 +1,53 @@
 Spree::User.class_eval do
   after_create :add_to_newsletter
+  after_update :check_newsletter
 
   private
 
     def add_to_newsletter
       # Add check for acceptance of newsletter subscription
-      # TODO
       if newsletter_optin
         Gibbon::API.lists.subscribe(
           {
             :id => ENV['MAILCHIMP-LIST-ID'],
             :email => {:email => self.email},
-            :double_optin => false
+            :double_optin => false,
+            :update_existing => true
           })
       end
+    end
+
+    def remove_from_newsletter
+      if !newsletter_optin
+        Gibbon::API.lists.unsubscribe(
+          {
+            :id => ENV['MAILCHIMP-LIST-ID'],
+            :email => {:email => self.email},
+            :delete_member => true
+          })
+      end
+    end
+
+    def check_newsletter
+      # Check whether on update the user has modified a preference
+      subscribers = Gibbon::API.lists.member_info(
+        {
+          :id => ENV['MAILCHIMP-LIST-ID'],
+          :emails => [{:email => self.email}]
+        })
+
+      if newsletter_optin
+        if subscribers["success_count"] < 1
+          # puts "Adding #{self.email} to newletter"
+          add_to_newsletter
+        end
+      else
+        if subscribers["success_count"] >= 1
+          # puts "Removing #{self.email} from newletter"
+          remove_from_newsletter
+        end
+      end
+
     end
 
 end
