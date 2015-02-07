0e15b96661e5c4c0279893187b8b021acb6bb3cb
ilnico <nico@niccolosanarico.com>
2015-02-07 18:43:03 +0000

    Bug fixing readying for production

--- app/models/spree/calculator/shipping/sda.rb
+++ app/models/spree/calculator/shipping/sda.rb
@@ -3,8 +3,6 @@ require_dependency 'spree/shipping_calculator'
 module Spree
   module Calculator::Shipping
     class Sda < ShippingCalculator
-      preference :costo, :decimal, default: 0
-      preference :colli, :integer, default: 6
 
       def self.description
         "Tariffa SDA"
@@ -17,7 +15,7 @@ module Spree
       def compute_from_weight(weight)
         #self.preferred_costo * ( (weight / preferred_colli).floor + 1)
         puts weight
-        
+
         case weight
         when 0...3
           10
--- vendor/assets/javascripts/spree/frontend/checkout_confirm.js
+++ vendor/assets/javascripts/spree/frontend/checkout_confirm.js
@@ -2,6 +2,7 @@
 // Checkout verification that the user has accepted terms and conditions
 
 $(document).ready(function() {
+  $("#accept_terms").prop("checked", false); // Uncheck the checkbox on page ready
   $("#accept_terms").click(function() {
     $("#checkout_confirm_button").attr("disabled", !this.checked);
   });
