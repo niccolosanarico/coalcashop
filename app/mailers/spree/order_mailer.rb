2b1f50d7a0edd37376e2c5aca19238fa9c181db9
ilnico <nico@niccolosanarico.com>
2015-02-16 20:28:16 +0000

    Added env variable for ecommerce email

--- app/mailers/spree/order_mailer.rb
+++ app/mailers/spree/order_mailer.rb
@@ -4,7 +4,7 @@ module Spree
       @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
       subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
       subject += "#{Spree::Config[:site_name]} - #{Spree.t('order_mailer.confirm_email.subject')} ##{@order.number}"
-      mail(to: @order.email, from: from_address, subject: subject)
+      mail(to: @order.email, bcc: ENV["ECOMMERCE_MAIL"], from: from_address, subject: subject)
     end
 
     def approve_email(order, resend = false)
