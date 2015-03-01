a1763175d572d8b7e72c2118e31d7ec7f8c490f8
ilnico <nico@niccolosanarico.com>
2015-03-01 15:30:50 +0000

    Added FAQ to main menu, fixed email design

--- app/overrides/img_popup_in_product_details.rb
+++ app/overrides/img_popup_in_product_details.rb
@@ -1,4 +1,16 @@
 Deface::Override.new(:virtual_path => 'spree/products/_image',
                      :name => 'img_popup_in_product_details',
-                     :replace => 'erb[loud]:contains("image_tag image.attachment.url(:product)")',
-                     :partial => 'shared/image_add')
+                     :insert_after => 'erb[loud]:contains("image_tag")',
+                     :text => %q{
+
+                       <div id="img" class="modal fade" tabindex="-1" role="dialog">
+                         <div class="modal-dialog">
+                           <div class="modal-content">
+                             <div class="modal-body">
+                               <%= image_tag image.attachment.url(:large), :itemprop => "image", :class => "img-responsive" %>
+                             </div>
+                           </div>
+                         </div>
+                       </div>
+
+                     })
