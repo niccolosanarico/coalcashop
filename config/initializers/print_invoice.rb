f8f3abecf9a2c4522b2f46d824cc5841d6945e7b
Niccolo Sanarico <nico@niccolosanarico.com>
2015-10-29 12:39:40 +0100

    typo error

--- config/initializers/print_invoice.rb
+++ config/initializers/print_invoice.rb
@@ -1,6 +1,5 @@
 Spree::PrintInvoice::Config.set(
     page_layout: :portrait,
     page_size: 'A4',
-    logo_path: 'images/Coalca_Small.jpg',
-    logo_scale:
+    logo_path: 'images/Coalca_Small.jpg'
 )
