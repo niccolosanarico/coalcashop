8881e991ae0c6a65655d09ceebf0e071672bbadf
Niccolo Sanarico <nico@niccolosanarico.com>
2015-10-30 12:51:56 +0100

    Switched to CDN for attachments

--- app/mailers/spree/order_mailer.rb
+++ app/mailers/spree/order_mailer.rb
@@ -11,7 +11,7 @@ module Spree
       @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
       subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
       subject += "#{Spree::Store.current.name} - Presa in carico del tuo ordine ##{@order.number}"
-      attachments['Coalca Shop - condizioni di vendita.pdf'] = File.read("#{Rails.root}/public/assets/Condizioni\ Generali\ di\ Vendita\ -\ Coalca\ Shop.pdf")
+      attachments['Coalca Shop - condizioni di vendita.pdf'] = File.read("https://coalca.blob.core.windows.net/cdn/Condizioni Generali di Vendita - Coalca Shop.pdf")
       mail(to: @order.email, from: from_address, subject: subject)#, attachments: attachments)
     end
 
