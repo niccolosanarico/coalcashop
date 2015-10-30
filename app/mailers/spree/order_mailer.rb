db2b9ae02ea2b71867d8002a076e6f4b0c473cca
Niccolo Sanarico <nico@niccolosanarico.com>
2015-10-30 12:58:00 +0100

    Fixing attachments from URL

--- app/mailers/spree/order_mailer.rb
+++ app/mailers/spree/order_mailer.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 module Spree
   class OrderMailer < BaseMailer
     def confirm_email(order, resend = false)
@@ -11,7 +13,7 @@ module Spree
       @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
       subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
       subject += "#{Spree::Store.current.name} - Presa in carico del tuo ordine ##{@order.number}"
-      attachments['Coalca Shop - condizioni di vendita.pdf'] = File.read("https://coalca.blob.core.windows.net/cdn/Condizioni Generali di Vendita - Coalca Shop.pdf")
+      attachments['Coalca Shop - condizioni di vendita.pdf'] = open("https://coalca.blob.core.windows.net/cdn/Condizioni Generali di Vendita - Coalca Shop.pdf").read
       mail(to: @order.email, from: from_address, subject: subject)#, attachments: attachments)
     end
 
