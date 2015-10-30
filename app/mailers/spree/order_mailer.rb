5bf4373396f367e12681c82e00b2d63ae451da0f
Niccolo Sanarico <nico@niccolosanarico.com>
2015-10-30 14:34:13 +0100

    Fixing url

--- app/mailers/spree/order_mailer.rb
+++ app/mailers/spree/order_mailer.rb
@@ -13,7 +13,7 @@ module Spree
       @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
       subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
       subject += "#{Spree::Store.current.name} - Presa in carico del tuo ordine ##{@order.number}"
-      attachments['Coalca Shop - condizioni di vendita.pdf'] = URI::open(URI::encode("https://coalca.blob.core.windows.net/cdn/Condizioni Generali di Vendita - Coalca Shop.pdf")).read
+      attachments['Coalca Shop - condizioni di vendita.pdf'] = open(URI::encode("https://coalca.blob.core.windows.net/cdn/Condizioni Generali di Vendita - Coalca Shop.pdf")).read
       mail(to: @order.email, from: from_address, subject: subject)#, attachments: attachments)
     end
 
