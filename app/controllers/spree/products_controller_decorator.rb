0572f91feb979d9d4524a4e16bcde55a1fb72d14
Niccolo Sanarico <nico@niccolosanarico.com>
2016-05-30 22:12:32 +0200

    Added sorting by price

---
+++ app/controllers/spree/products_controller_decorator.rb
@@ -0,0 +1,29 @@
+module Spree
+  ProductsController.class_eval do
+      helper_method :sorting_param
+      alias_method :old_index, :index
+
+      def index
+        old_index # Like calling super: http://stackoverflow.com/a/13806783/73673
+        @products = @products.send(sorting_scope)
+      end
+
+      def sorting_param
+        params[:sorting].try(:to_sym) || default_sorting
+      end
+
+      private
+
+      def sorting_scope
+        allowed_sortings.include?(sorting_param) ? sorting_param : default_sorting
+      end
+
+      def default_sorting
+        :ascend_by_updated_at
+      end
+
+      def allowed_sortings
+        [:descend_by_master_price, :ascend_by_master_price]
+      end
+    end
+end
---
+++ app/helpers/spree/products_helper_decorator.rb
@@ -0,0 +1,18 @@
+module Spree
+  ProductsHelper.module_eval do
+    ##
+    # Override cache_key_for_products to add caching per sort param.
+    def cache_key_for_products
+      count = @products.count
+      # Instead of default max_updated_at, we look at the first product in the list
+      # And we add sorting, so that we get a product-cache per sorting param
+      first_id = @products.first.id
+      sorting = params[:sorting]
+      "#{I18n.locale}/#{current_currency}/spree/products/all-#{params[:page]}-#{first_id}-#{sorting}-#{count}"
+    end
+
+    def current_sorting?(key)
+      sorting_param == key.to_sym
+    end
+  end
+end
