c0cff19f01e4ef727e5022b6dd123e74559a9273
Niccolo Sanarico <nico@niccolosanarico.com>
2016-10-25 00:35:36 +0200

    removed highlight from side menu

--- app/helpers/spree/frontend_helper_decorator.rb
+++ app/helpers/spree/frontend_helper_decorator.rb
@@ -7,7 +7,7 @@ module Spree
       content_tag :div, class: 'list-group' do
         taxons = root_taxon.children.map do |taxon|
           # css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'list-group-item active' : 'list-group-item'
-          css_class = (current_taxon.name == taxon.name) ? 'list-group-item active' : 'list-group-item'
+          css_class = (current_taxon.name == taxon.name) ? 'list-group-item' : 'list-group-item'
           link_to(taxon.name, seo_url(taxon), class: css_class) + taxons_tree(taxon, current_taxon, max_level - 1)
         end
         safe_join(taxons, "\n")
