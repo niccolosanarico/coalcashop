af4b429e4a0d3eee93d306cda65594defb52b5bc
Niccolo Sanarico <nico@niccolosanarico.com>
2016-10-24 22:50:44 +0200

    trying to fix helpers

--- app/helpers/spree/frontend_helper_decorator.rb
+++ app/helpers/spree/frontend_helper_decorator.rb
@@ -1,17 +1,15 @@
-module Spree
-  FrontendHelper.module_eval do
-    ##
-    # Override taxons_tree to use the correct css class highlighting rules
-    def taxons_tree(root_taxon, current_taxon, max_level = 1)
-      return '' if max_level < 1 || root_taxon.leaf?
-      content_tag :div, class: 'list-group' do
-        taxons = root_taxon.children.map do |taxon|
-          # css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'list-group-item active' : 'list-group-item'
-          css_class = (current_taxon.name == taxon.name) ? 'list-group-item active' : 'list-group-item'
-          link_to(taxon.name, seo_url(taxon), class: css_class) + taxons_tree(taxon, current_taxon, max_level - 1)
-        end
-        safe_join(taxons, "\n")
+Spree::FrontendHelper.module_eval do
+  ##
+  # Override taxons_tree to use the correct css class highlighting rules
+  def taxons_tree(root_taxon, current_taxon, max_level = 1)
+    return '' if max_level < 1 || root_taxon.leaf?
+    content_tag :div, class: 'list-group' do
+      taxons = root_taxon.children.map do |taxon|
+        # css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'list-group-item active' : 'list-group-item'
+        css_class = (current_taxon.name == taxon.name) ? 'list-group-item active' : 'list-group-item'
+        link_to(taxon.name, seo_url(taxon), class: css_class) + taxons_tree(taxon, current_taxon, max_level - 1)
       end
+      safe_join(taxons, "\n")
     end
   end
 end
