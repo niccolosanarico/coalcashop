bab99defd7451b55b859243583d66789ab7358aa
Niccolo Sanarico <nico@niccolosanarico.com>
2016-10-26 01:04:03 +0200

    fixing side menu highlighting

--- app/helpers/spree/frontend_helper_decorator.rb
+++ app/helpers/spree/frontend_helper_decorator.rb
@@ -6,7 +6,6 @@ module Spree
 
       return '' if max_level < 1 || root_taxon.leaf?
       content_tag :div, class: 'list-group' do
-        console
         taxons = root_taxon.children.map do |taxon|
           css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'list-group-item active' : 'list-group-item'
           # css_class = (current_taxon.name == taxon.name) ? 'list-group-item' : 'list-group-item'
