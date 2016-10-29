f3475d4006996f8a294e8afee8d3328fe51c963a
Niccolo Sanarico <nico@niccolosanarico.com>
2016-10-29 18:08:44 +0200

    bundle update & added fontawesome to cart delete

---
+++ app/overrides/change_line_item_add_minus.rb
@@ -0,0 +1,8 @@
+Deface::Override.new(:virtual_path => 'spree/orders/_line_item',
+                     :name => 'change_line_item_add_minus',
+                     :replace => 'td.cart-item-delete',
+                     :text => %q(
+                       <td class="cart-item-delete" data-hook="cart_item_delete">
+                         <%= link_to content_tag(:span, '', class: 'fa fa-minus-circle'), '#', class: 'delete', id: "delete_#{dom_id(line_item)}" %>
+                      </td>
+                      )
